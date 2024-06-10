819461f38875c715141fc3fe28eb1d7d8fe6ee7b
user.name <akamrannicole@gmail.com>
2024-06-10 10:30:07 +0300

    done

--- lib/debug.py
+++ lib/debug.py
@@ -1,9 +1,16 @@
-#!/usr/bin/env python3
-#lib/testing/debug.py
-
 from __init__ import CONN, CURSOR
 from department import Department
 
 import ipdb
 
+
+def reset_database():
+    Department.drop_table()
+    Department.create_table()
+
+    Department.create("Payroll", "Building A, 5th Floor")
+    Department.create("Human Resources", "Building C, East Wing")
+    Department.create("Accounting", "Building B, 1st Floor")
+
+reset_database()
 ipdb.set_trace()
